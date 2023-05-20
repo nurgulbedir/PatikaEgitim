81521b6dee7b6426b09fe947154bc28366a835b9
Nurgül Bedir <nurgulbedir5@gmail.com>
2023-05-20 21:12:09 +0300

    Add files via upload

---
+++ Java101/src/BasamakToplam.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BasamakToplam {
+    public static void main(String[] args) {
+        //Bir sayının basamak sayılarının toplamını hesaplayan program yazınız. Örnek : 1643 = 1 + 6 + 4 + 3 = 14
+        Scanner k=new Scanner(System.in);
+        int toplam=0 ;
+        System.out.print("Sayı giriniz: ");
+        int sayi=k.nextInt();
+        int yeniSayi=sayi;
+
+        while (yeniSayi != 0) {
+            int basamak = yeniSayi % 10;
+            toplam += basamak;
+            yeniSayi /= 10;
+        }
+        System.out.println("Basamaklar toplamı: " + toplam);
+    }
+}
