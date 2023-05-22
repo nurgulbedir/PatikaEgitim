b8ae9b5af1718be7bdd2c0a4a4c926e591bab3bc
Nurgül Bedir <nurgulbedir5@gmail.com>
2023-05-22 17:57:16 +0300

    Add files via upload

---
+++ Java101/src/ArtikYil.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ArtikYil {
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        System.out.print("Yıl giriniz: ");
+        int yil = k.nextInt();
+
+                if (yil % 4 == 0 && yil % 100 != 0 || yil % 400 == 0) System.out.println(yil + " bir artık yıldır !");
+                else System.out.println(yil+" bir artık yıl değildir !");
+        }
+    }
+
